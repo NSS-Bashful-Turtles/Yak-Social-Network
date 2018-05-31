dfe82a3d6d7a990add693f9486ded53f8161b5e5
Riley Mathews <rileymathews80@gmail.com>
2018-05-31 14:23:13 -0500

    split profile header into own component

--- src/profile/ProfileView.js
+++ src/profile/ProfileView.js
@@ -1,17 +1,18 @@
 import React, { Component } from 'react'
-import { Hero, Container, HeroBody, Title, Box } from 'bloomer';
+import { Hero, Container, HeroBody, Title, Box, Button, Section } from 'bloomer';
 import './Profile.css'
 import Posts from '../newsfeed/posts';
+import ProfileHeader from './ProfileHeader';
 
-
-class UserProfile extends Component {
+class ProfileView extends Component {
     // set default data for the component
     // if xhr request does not complete the default information will let you know
     constructor(props) {
         super(props)
 
         this.state = {
-            userId: "FakerUserName",
+            id: "",
+            username: "FakerUserName",
             name: {
                 first: "if you are",
                 last: "seeing this it means",
@@ -19,7 +20,8 @@ class UserProfile extends Component {
             email: "fake@fakesite.fake",
             location: "api request did not complete",
             image: "https://placeimg.com/200/200/people",
-            recentPosts: []
+            recentPosts: [],
+            isCurrentUser: false
         }
     }
 
@@ -31,13 +33,25 @@ class UserProfile extends Component {
 
             //set properties that are provided by the user request
             .then(response => {
+                console.log(response)
                 this.setState({
-                    userId: response.userId,
+                    id: response.id,
+                    username: response.username,
                     name: response.name,
                     email: response.email,
                     location: response.location,
-                    image: response.image
+                    image: response.image,
                 })
+                if (this.state.id === sessionStorage.getItem("userId")) {
+                    this.setState({
+                        isCurrentUser: true
+                    })
+                } else {
+                    this.setState({
+                        isCurrentUser: false
+                    })
+                }
+                console.log(this.state.isCurrentUser)
             })
 
         //fetch the 5 most recent posts by the user and update the response array in the component state
@@ -56,24 +70,24 @@ class UserProfile extends Component {
     render() {
         return (
             <div>
-                <Hero>
-                    <HeroBody>
-                        <Container>
-                            <img className="is-pulled-left" src={this.state.image}/>
-                            <Title>Name: {this.state.name.first} {this.state.name.last}</Title>
-                            <p>Location: {this.state.location}</p>
-                        </Container>
-                    </HeroBody>
-                </Hero>
+                <ProfileHeader
+                    image={this.state.image}
+                    first={this.state.name.first}
+                    last={this.state.name.last}
+                    location={this.state.location}
+                />
+                <Section>
+                    <Button disabled="true">Change Theme</Button>
+                </Section>
                 <Box>
                     <Title>Recent Posts</Title>
-                {this.state.recentPosts.map(p => (
-                    <Posts image={p.image} content={p.content} key={p.id}/>
-                ))}
+                    {this.state.recentPosts.map(p => (
+                        <Posts image={p.image} content={p.content} key={p.id} />
+                    ))}
                 </Box>
             </div>
         )
     }
 }
 
-export default UserProfile
+export default ProfileView
