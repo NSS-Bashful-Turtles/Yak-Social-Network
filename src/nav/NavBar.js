dde200ac53be13b99acd090f015c02d134fa376a
Riley Mathews <rileymathews80@gmail.com>
2018-06-01 10:04:51 -0500

    made navbar dropdown work

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -2,28 +2,47 @@ import React, { Component } from "react";
 import { Route, Link } from "react-router-dom";
 
 // Using bloomer tags to import bulma styling
-import {Navbar, NavbarItem, Input, Control, Button} from "bloomer";
+import { Navbar, NavbarItem, Input, Control, Button, NavbarBurger, NavbarBrand, NavbarMenu } from "bloomer";
 import 'bulma/css/bulma.css'
 
 class NavBar extends Component {
-// Storing session storage as an object in state named currentUser
-    state = {
-        currentUser: sessionStorage.getItem('userId')
+
+    constructor(props) {
+        super(props)
+        // Storing session storage as an object in state named currentUser
+        this.state = {
+            currentUser: sessionStorage.getItem('userId'),
+            isActive: false
+        }
     }
 
-// Line 24: Session user is grabbed and profile page is loaded upon profile click
+    // event handler for clicking nav drop down burger
+    // sets isActive property in state to the opposite of what it currently is
+    onClickNav = function() {
+        this.setState({
+            isActive: (!this.state.isActive)
+        })
+    }.bind(this)
+
+    // Line 24: Session user is grabbed and profile page is loaded upon profile click
     render() {
         return (
-        <Navbar>
-            <NavbarItem href={"/home/" + this.state.currentUser}>Home</NavbarItem>
-            <Control>
-                <Input type="text" placeholder="Search"></Input>
-            </Control>
+            <Navbar>
+                <NavbarBrand>
+                    <NavbarItem>Yak</NavbarItem>
+                    <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
+                </NavbarBrand>
+                <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
+                <NavbarItem href={"/home/" + this.state.currentUser}>Home</NavbarItem>
+                <Control>
+                    <Input type="text" placeholder="Search"></Input>
+                </Control>
                 <Button isColor="info" isOutlined>S</Button>
-            <NavbarItem href="/">Notifications</NavbarItem>
-            <NavbarItem href={"/profile/" + this.state.currentUser}>Jacob</NavbarItem>
-            <NavbarItem href={"/"}>Logout</NavbarItem>
-        </Navbar>
+                    <NavbarItem href="/">Notifications</NavbarItem>
+                    <NavbarItem href={"/profile/" + this.state.currentUser}>Jacob</NavbarItem>
+                    <NavbarItem href={"/"}>Logout</NavbarItem>
+                </NavbarMenu>
+            </Navbar>
         )
     }
 }
