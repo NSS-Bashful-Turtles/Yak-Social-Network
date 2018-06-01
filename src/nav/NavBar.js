4889542ae501efbfd42882f154ead06070b026d3
Riley Mathews <rileymathews80@gmail.com>
2018-06-01 11:09:25 -0500

    added sepearte function for search button

--- src/nav/NavBar.js
+++ src/nav/NavBar.js
@@ -18,15 +18,15 @@ class NavBar extends Component {
         }
     }
 
-// Making a fetch request against sessionStorage to find relevant user and storing first name in state
+    // Making a fetch request against sessionStorage to find relevant user and storing first name in state
     componentDidMount() {
         fetch(`http://127.0.0.1:8088/users/${this.state.currentUser}`)
-        .then(r => r.json())
-        .then(response => {
-            this.setState({
-                firstName: response.name.first
+            .then(r => r.json())
+            .then(response => {
+                this.setState({
+                    firstName: response.name.first
+                })
             })
-        })
     }
 
     // event handler for clicking nav drop down burger
@@ -37,6 +37,17 @@ class NavBar extends Component {
         })
     }.bind(this)
 
+    //on click of search button
+    onClickSearch = function () {
+        //fire function to close navbar
+        this.onClickNav()
+
+        /* 
+            add code here 
+            to fire search functionality
+        */
+    }.bind(this)
+
     // Line 24: Session user is grabbed and profile page is loaded upon profile click
     render() {
         return (
@@ -45,15 +56,13 @@ class NavBar extends Component {
                     <NavbarItem>Yak</NavbarItem>
                     <NavbarBurger isActive={this.state.isActive} onClick={this.onClickNav} />
                 </NavbarBrand>
-                <NavbarMenu isActive={this.state.isActive} onClick={this.onClickNav}>
-                    <NavbarItem href={"/home/" + this.state.currentUser}>Home</NavbarItem>
-                    <Control>
-                        <Input id="input__search" type="text" placeholder="Search"></Input>
-                        <Button isColor="info" isOutlined>S</Button>
-                    </Control>
-                    <NavbarItem href="/">Notifications</NavbarItem>
-                    <NavbarItem href={"/profile/" + this.state.currentUser}>{this.state.firstName}</NavbarItem>
-                    <NavbarItem href={"/"}>Logout</NavbarItem>
+                <NavbarMenu isActive={this.state.isActive}>
+                    <NavbarItem href={"/home/" + this.state.currentUser} onClick={this.onClickNav}>Home</NavbarItem>
+                    <Input id="input__search" type="text" placeholder="Search" ></Input>
+                    <Button isColor="info" onClick={this.onClickSearch} isOutlined>S</Button>
+                    <NavbarItem href="/" onClick={this.onClickNav}>Notifications</NavbarItem>
+                    <NavbarItem href={"/profile/" + this.state.currentUser} onClick={this.onClickNav}>{this.state.firstName}</NavbarItem>
+                    <NavbarItem href={"/"} onClick={this.onClickNav}>Logout</NavbarItem>
                 </NavbarMenu>
             </Navbar>
         )
